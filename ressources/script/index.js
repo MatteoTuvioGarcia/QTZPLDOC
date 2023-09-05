464fcac9838efd1f3d11d0ee8ba7f3840c05c109
Matteo Garcia <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2023-09-05 14:49:56 +0200

    working sidebar

--- ressources/script/index.js
+++ ressources/script/index.js
@@ -1,7 +1,7 @@
-head = document.getElementById("mainhead")
-head.addEventListener("click", clkhandler, false)
+// head = document.getElementById("mainhead")
+// head.addEventListener("click", clkhandler, false)
 
-function clkhandler(evt){
-    handler = document.getElementById(evt.currentTarget.id)
-   handler.innerText = 'STOP CLICKING ME'
-}
+// function clkhandler(evt){
+//     handler = document.getElementById(evt.currentTarget.id)
+//    handler.innerText = 'STOP CLICKING ME'
+// }
