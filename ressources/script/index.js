65e7c54a72d29faebeef951d03bc1f79c7684e53
Matteo Garcia <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2023-09-05 14:22:19 +0200

    formatted directories

---
+++ ressources/script/index.js
@@ -0,0 +1,7 @@
+head = document.getElementById("mainhead")
+head.addEventListener("click", clkhandler, false)
+
+function clkhandler(evt){
+    handler = document.getElementById(evt.currentTarget.id)
+   handler.innerText = 'STOP CLICKING ME'
+}
