e6a3ac89e026eb53163311e88c2bdfd30e150ae4
Matteo Garcia <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2023-09-08 15:23:25 +0200

    autoloading label files

--- ressources/script/preload.js
+++ ressources/script/preload.js
@@ -20,21 +20,63 @@ function loadPage(href) {
         console.log('Aded')
         clickme.addEventListener("click", fctClicked, false)
     }
-    elements = document.getElementsByClassName("zplsourcefile");
+    zplPage = document.getElementById("zplWrapper");
 
-    if (elements != null || elements.length != 0) {
-        for (var i = 0; i < elements.length; i++)
-        {
-            loadZplElement(elements[i])
-        }
+    if (zplPage != null) {
+        loadZplElement(zplPage)
+
     }
 }
+
 function loadZplElement(element) {
-        let currentId = element.id
+
+
+    let zplData;
+    let zplPrefix = 'label';
+    let zplSuffix = '.ZPL';
+    let constructedFileName;
+    let I = 1;
+    let stop = false;
+    while (I <= 4) {
+
+        constructedFileName = zplPrefix + I + zplSuffix;
+        console.log(constructedFileName)
         $.ajax({
-            url: 'ressources/zplscripts/' + currentId + ".ZPL",
+            url: 'ressources/zplscripts/' + constructedFileName,
             success: function (data) {
-                element.innerText = data;
+                let count = document.getElementsByClassName("card_custom").length + 1
+                let newLabel = document.createElement('div')
+                let labelName = data.substring(1, data.search('\n')).substring(18,70)
+                let filename = data.substring(1, data.search('\n')).substring(3,18)
+                console.log(filename)
+                console.log(labelName)
+                data = data.substring(data.search('\n') + 1, data.length)
+                newLabel.innerHTML = "<div class=\"border row rounded card_custom\">\n" +
+                    "    <div class=\"bg-dark text-light rounded p-2\">\n" +
+                    "        <h2>Snippet " + count +": "+ labelName +"</h2>\n" +
+                    "    </div>\n" +
+                    "    <div class=\"bg-light pt-2 card_custom_inside\">\n" +
+                    "        <div class=\"zplcode\">\n" +
+                    "            <p class=\"zplsourcefile\" id=\"label2\">\n" +
+                    data.replaceAll("\n", "<br/>") +
+                    "            </p>\n" +
+                    "        </div>\n" +
+                    "        <a href=\"ressources/zplscripts/" + filename + "\" download>\n" +
+                    "            <button type=\"button\" class=\"btn btn-dark\">\n" +
+                    "                <span>download as .ZPL</span>\n" +
+                    "            </button>\n" +
+                    "        </a>\n" +
+                    "    </div>\n" +
+                    "</div>"
+                element.append(newLabel)
             }
+
         });
+        I++;
+    }
+
+}
+
+function readZpl(id) {
+
 }
